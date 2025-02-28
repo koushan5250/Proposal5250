f67416edd739837452303345eff7eee4827777bd
koushan5250 <koushanshaikh@gmail.com>
2025-03-01 01:16:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 function startGame() {
     alert("Let's start the game! ❤️");
-    window.location.href = "question1.html"; // Next Page
+    window.location.href = "page2.html"; // First question page
 }
 
-// Tracking System Example
+// Function to navigate to the next page
+function goToNextPage(currentPage) {
+    let nextPage = "";
+    if (currentPage === "page2.html") {
+        nextPage = "page3.html";
+    } else if (currentPage === "page3.html") {
+        nextPage = "page4.html";
+    } else if (currentPage === "page4.html") {
+        nextPage = "page5.html";
+    } else if (currentPage === "page5.html") {
+        nextPage = "final.html"; // Last page
+    }
+
+    if (nextPage) {
+        window.location.href = nextPage;
+    }
+}
+
+// Tracking system to log user clicks
 document.addEventListener("click", function(event) {
     console.log("User clicked on:", event.target.innerText);
 });
